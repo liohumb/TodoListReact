248fd5cda4b7587c4e565bee62401f16d1337cb7
liohumb <helloliohumb@gmail.com>
2021-10-18 11:26:31 +0200

    création de l'interface

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Form from "./components/Form/Form";
 
 function App() {
   return (
     <div className="App">
-
+      <h1 className="text-center mt-3">Todo—List</h1>
+        <Form/>
     </div>
   );
 }
